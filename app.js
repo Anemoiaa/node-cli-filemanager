b2894c788c8023b7c37148f12d2ab0828c741498
Anemoiaa <georgiypetroff@gmail.com>
2022-06-08 14:01:46 +0400

    feat: add programm exit listeners

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+import  {EventEmitter} from 'events';
+import os from 'os';
 import { parseUsername } from './utils/parseUsername.js';
 
 const welcomeUser = (username) => {
-	process.stdout.write(`Welcome to the File Manager, ${username}!`);
+	process.stdout.write(`Welcome to the File Manager, ${username}!\n`);
 } 
 
 const username = parseUsername(process.argv[2]);
 welcomeUser(username);
+
+
+const emitter = new EventEmitter();
+
+emitter.once('.exit', () => {
+	process.stdout.write(`Thank you for using File Manager, ${username}!`);
+	process.exit(0);
+});
+
+process.on('SIGINT', () => emitter.emit('.exit'));
+
+process.stdin.on('data', (data) => {
+	const [command, ...args] = data.toString().replace(os.EOL, '').replace('\n', '').split(' ');
+	emitter.emit(command, args);
+});
