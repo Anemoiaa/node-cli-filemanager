0e31838d750e09ce28b71d422e125e781d4e47af
Anemoiaa <georgiypetroff@gmail.com>
2022-06-10 19:22:04 +0400

    refactor: minor changes

--- calcHash.js
+++ calcHash.js
@@ -1,16 +1,22 @@
 import crypto from 'crypto';
 import fs from 'fs';
 import path from 'path';
+import { displayCurrentDir } from './utils/displayCurrentDir.js';
 
 export const calculateHash = async (currentDir, pathToFile, cb) => {
-    pathToFile = path.isAbsolute(pathToFile) ?  path.normalize(pathToFile) : path.join(currentDir, pathToFile);
+    try {
+        pathToFile = path.isAbsolute(pathToFile) ?  path.normalize(pathToFile) : path.join(currentDir, pathToFile);
+    } catch {
+        console.error("Operation failed");
+        displayCurrentDir(currentDir);
+        return;
+    }
     const file = fs.readFile(pathToFile, 'utf-8', (err, buffer) => {
-        if (err) {
-            console.error("Operation failed");
-            return;
+        if (err) console.error("Operation failed");
+        else {
+            const hash = crypto.createHash('sha256').update(buffer);
+            console.log(hash.digest('hex')); 
         }
-        const hash = crypto.createHash('sha256').update(buffer);
-        console.log(hash.digest('hex'));
-        cb();    
+        displayCurrentDir(currentDir);
     });
 };
